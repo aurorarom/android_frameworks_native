c2a94fc7d793cff6874d435bcc136050686043d2
Adriano Martins <adrianomartins@gmail.com>
2016-10-09 04:39:10 -0300

    binder: disable all warnings being treated as errors
    Change-Id: I1f1bd9a64bbf320dfb2ff9bc867fadfb28eed5f7

--- libs/binder/Android.mk
+++ libs/binder/Android.mk
@@ -78,7 +78,7 @@ ifneq ($(TARGET_IS_64_BIT),true)
 LOCAL_CFLAGS += -DBINDER_IPC_32BIT=1
 endif
 endif
-LOCAL_CFLAGS += -Werror
+LOCAL_CFLAGS += -Wno-error
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
